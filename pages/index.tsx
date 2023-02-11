f8d090d4f7649b01e1408bfed464ba69990fbeb6
Egbert HU <62612263+gottaegbert@users.noreply.github.com>
2023-02-11 16:37:13 +0100

    fix spline

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ import FloatingLink from "../components/FloatingLink/FloatingLink";
 import StaggeredTitle from "../components/StaggeredTitle/StaggeredTitle";
 import CaseStudy from "../components/CaseStudy/CaseStudy";
 import Work from "../components/Work/Work";
-import { SplineD } from "../components/SplineD/SplineD"
+// import { SplineD } from "../components/SplineD/SplineD"
 import Link from "next/link";
 import { GetStaticProps } from "next";
 import Cursor from "../components/Cursor/Cursor";
@@ -79,7 +79,7 @@ const IndexPage: React.FC<Props> = ({ data }) => {
         
         <BasicMeta url={"/"} />
         <div className={cn(styles.heroContainer)}>
-            <SplineD/>
+            {/* <SplineD/> */}
           <section className={cn("grid")}>
             <div
               className={cn(
