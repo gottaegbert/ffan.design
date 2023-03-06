4d663997ad16cb147df3c40318cc2e7141bf0f1b
Egbert HU <62612263+gottaegbert@users.noreply.github.com>
2023-03-06 23:38:00 +0100

    add vercel audiences

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
 import "../styles/global.scss";
 import type { AppProps } from "next/app";
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
+  <Analytics />
 }
 
 export default MyApp;
