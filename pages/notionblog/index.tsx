c674ffebaadbed431168e06a60d8206eae0f2789
Egbert HU <62612263+gottaegbert@users.noreply.github.com>
2023-04-29 14:19:23 +0200

    update

--- pages/notionblog/index.tsx
+++ pages/notionblog/index.tsx
@@ -5,33 +5,11 @@ import { ExtendedRecordMap } from 'notion-types'
 // import { NotionPages } from '../../components/NotionPages/NotionPages'
 import { rootNotionPageId } from '../../lib/config'
 import notion from '../../lib/notion'
-import dynamic from 'next/dynamic'
+
 import { NotionRenderer } from 'react-notion-x'
 import Link from 'next/link'
 
-const Code = dynamic(() =>
-    import('react-notion-x/build/third-party/code').then((m) => m.Code)
-)
-const Collection = dynamic(() =>
-    import('react-notion-x/build/third-party/collection').then(
-        (m) => m.Collection
-    )
-)
-const Equation = dynamic(() =>
-    import('react-notion-x/build/third-party/equation').then((m) => m.Equation)
-)
-const Pdf = dynamic(
-    () => import('react-notion-x/build/third-party/pdf').then((m) => m.Pdf),
-    {
-        ssr: false
-    }
-)
-const Modal = dynamic(
-    () => import('react-notion-x/build/third-party/modal').then((m) => m.Modal),
-    {
-        ssr: false
-    }
-)
+
 
 export const getStaticProps = async () => {
     const pageId = rootNotionPageId
@@ -47,28 +25,7 @@ export const getStaticProps = async () => {
 
 export default function Page({ recordMap }: { recordMap: ExtendedRecordMap }) {
     return <NotionRenderer recordMap={recordMap}
-        components={{
-            PageLink: (props: {
-                href: any;
-                children:
-                | string
-                | number
-                | boolean
-                | React.ReactElement<any, string | React.JSXElementConstructor<any>>
-                | React.ReactFragment
-                | React.ReactPortal
-                | null
-                | undefined;
-            }) => {
-                return <Link href={`/notionblog/${props.href}`}>{props.children}</Link>;
-            },
-            nextLink: Link,
-            Code,
-            Collection,
-            Equation,
-            Modal,
-            Pdf,
-        }}
+        fullPage={true}
         showCollectionViewDropdown={false}
         // searchNotion={config.isSearchEnabled ? searchNotion : null}
         rootPageId={rootNotionPageId} />
