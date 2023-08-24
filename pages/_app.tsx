bb26f658405e299c55efe764192f3d733e622911
Egbert HU <62612263+gottaegbert@users.noreply.github.com>
2023-08-24 14:39:04 +0200

    clean2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,7 @@
 import "../styles/global.scss";
 import type { AppProps } from "next/app";
 import { Analytics } from '@vercel/analytics/react';
-// import 'react-notion-x/src/styles.css'
 import './notionblog/notionblog.mudule.scss'
-// // used for code syntax highlighting (optional)
-// import 'prismjs/themes/prism-tomorrow.css'
-// // used for rendering equations (optional)
-// import 'katex/dist/katex.min.css'
 
 
 function MyApp({ Component, pageProps }: AppProps) {
