5ed67b3eeda960ff3dbc89630c5b426ffdbe6141
Egbert HU <62612263+gottaegbert@users.noreply.github.com>
2024-09-25 17:36:00 +0800

    update footer style

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -9,46 +9,45 @@ type Props = {}
 const Footer: React.FC<Props> = ({}) => {
     return (
         <footer className={styles.footer}>
-            <div className={'grid'}>
-                <div className={cn('col-12', styles.ctaContainer)}></div>
-                <div className={cn('col-12', styles.bottom)}>
-                    <Image
-                        src={logo}
-                        alt="FFAN Design Logo"
-                        className={'col-2'}
-                    />
-                    <div className={'col-3'}>
-                        <p className={'small'}>ffan.design</p>
+            <div className={`${'grid'} ${styles.bottom}`}>
+                <Image src={logo} alt="FFAN Design Logo" className={'col-1'} />
+                <div className={'col-4'}>
+                    <p className={'small'}>ffan.design</p>
+                    <br />
 
-                        <p className={'small'}>
-                            is an industrial and graphic design studio Based in
-                            Brooklyn & Beijing
-                        </p>
-                    </div>
-                    <div className={'col-3'}>
-                        <p className={'small bold'}>Contact Us</p>
-
-                        <p className={'small'}>Email</p>
-                        <p className={'small'}>info@ffandesign.com</p>
-
-                        <p className={'small'}>Instagram</p>
-                        <p className={'small'}>ffan.design</p>
-                        <p className={'small'}>Phone</p>
-                        <p className={'small'}>+86 13260458928</p>
+                    <p className={'small'}>
+                        is an industrial and graphic design studio Based in
+                        Brooklyn & Beijing
+                    </p>
+                </div>
+                <div className={'col-3'}>
+                    <p>Contact Us</p>
+                    <br />
+                    <div className={styles.twop}>
+                        <p>Email</p>
+                        <p>info@ffandesign.com</p>
                     </div>
 
-                    <div>
-                        <p className={'small'}>Address</p>
+                    <div className={styles.twop}>
+                        <p>Instagram</p>
+                        <p>@ffan.design</p>
+                    </div>
+                    <div className={styles.twop}>
+                        <p>Phone</p>
+                        <p>+86 13260458928</p>
+                    </div>
+                </div>
 
-                        <p className={'small'}>China</p>
-                        <p className={'small'}>
-                            Wangjingyuan D-602, Beijing. 100102
-                        </p>
-
-                        <p className={'small'}>US</p>
-                        <p className={'small'}>
-                            100 Steuben 4D, Brooklyn, NY 11205
-                        </p>
+                <div className={'col-4'}>
+                    <p>Address</p>
+                    <br />
+                    <div className={styles.twop}>
+                        <p>China</p>
+                        <p>Wangjingyuan D-602, Beijing. 100102</p>
+                    </div>
+                    <div className={styles.twop}>
+                        <p>US</p>
+                        <p>100 Steuben 4D, Brooklyn, NY 11205</p>
                     </div>
                 </div>
             </div>
