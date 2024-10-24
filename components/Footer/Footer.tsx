1a734da2080e37e1e9296fc3aa371a3d12e869da
Egbert HU <62612263+gottaegbert@users.noreply.github.com>
2024-10-24 19:50:11 +0800

    [mobile]-update work page

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -11,42 +11,47 @@ const Footer: React.FC<Props> = ({}) => {
             <div className={`${'grid'} ${styles.bottom}`}>
                 <Image src={logo} alt="FFAN Design Logo" className={'col-1'} />
                 <div className={'col-4'}>
-                    <p className={'small'}>ffan.design</p>
-                    <br />
-
-                    <p className={'small'}>
-                        is an industrial and graphic design studio Based in
-                        Brooklyn & Beijing
-                    </p>
+                    <div className={styles.singleLine}>
+                        <p className={'small'}>ffan.design</p>
+                        <br />
+                        <p className={'small'}>
+                            is an industrial and graphic design studio Based in
+                            Brooklyn & Beijing
+                        </p>
+                    </div>
                 </div>
                 <div className={'col-3'}>
-                    <p>Contact Us</p>
-                    <br />
-                    <div className={styles.contactInfo}>
-                        <div className={styles.labels}>
-                            <p>Email</p>
-                            <p>Instagram</p>
-                            <p>Phone</p>
-                        </div>
-                        <div className={styles.details}>
-                            <p>ffandesign.info@gmail.com</p>
-                            <p>@ffan.design</p>
-                            <p>+86 13260458928</p>
+                    <div className={styles.singleLine}>
+                        <p>Contact Us</p>
+                        <br />
+                        <div className={styles.contactInfo}>
+                            <div className={styles.labels}>
+                                <p>Email</p>
+                                <p>Instagram</p>
+                                <p>Phone</p>
+                            </div>
+                            <div className={styles.details}>
+                                <p>ffandesign.info@gmail.com</p>
+                                <p>@ffan.design</p>
+                                <p>+86 13260458928</p>
+                            </div>
                         </div>
                     </div>
                 </div>
 
                 <div className={'col-4'}>
-                    <p>Address</p>
-                    <br />
-                    <div className={styles.contactInfo}>
-                        <div className={styles.labels}>
-                            <p>China</p>
-                            <p>US</p>
-                        </div>
-                        <div className={styles.details}>
-                            <p>Wangjingyuan D-602, Beijing. 100102</p>
-                            <p>100 Steuben 4D, Brooklyn, NY 11205</p>
+                    <div className={styles.singleLine}>
+                        <p>Address</p>
+                        <br />
+                        <div className={styles.contactInfo}>
+                            <div className={styles.labels}>
+                                <p>China</p>
+                                <p>US</p>
+                            </div>
+                            <div className={styles.details}>
+                                <p>Wangjingyuan D-602, Beijing. 100102</p>
+                                <p>100 Steuben 4D, Brooklyn, NY 11205</p>
+                            </div>
                         </div>
                     </div>
                 </div>
